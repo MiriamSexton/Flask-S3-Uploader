96f5496fe6279027dcc6a40a0ced1207c435e239
Miriam Sexton <miriam@industrydive.com>
2013-05-22 17:17:59 -0400

    clean up config_defaults

--- config_defaults.py
+++ config_defaults.py
@@ -1,17 +1,11 @@
-import os
 import config_nonsecret
 
+# This is all stuff that should be set in the env:
 S3_LOCATION = 'http://your-amazon-site.amazonaws.com/'
 S3_KEY = 'YOURAMAZONKEY'
 S3_SECRET = 'YOURAMAZONSECRET'
 S3_UPLOAD_DIRECTORY = 'what_directory_on_s3'
 S3_BUCKET = 's3_bucket_name'
-
-#[('dir_value','friendly name'), ('dir_value', 'friendlyname')]
-if config.S3_UPLOAD_DIRECTORY_CHOICES and type(config.S3_UPLOAD_DIRECTORY_CHOICES) == type(list()):
-    S3_UPLOAD_DIRECTORY_CHOICES = config_nonsecret.S3_UPLOAD_DIRECTORY_CHOICES
-else:
-    S3_UPLOAD_DIRECTORY_CHOICES = [('','Root'),]
 
 USERNAME = 'username'
 PASSWORD = 'password'
@@ -20,3 +14,9 @@ PASSWORD = 'password'
 SECRET_KEY = "FLASK_SECRET_KEY"
 DEBUG = True
 PORT = 5000
+
+
+if config_nonsecret.S3_UPLOAD_DIRECTORY_CHOICES and type(config_nonsecret.S3_UPLOAD_DIRECTORY_CHOICES) == type(list()):
+    S3_UPLOAD_DIRECTORY_CHOICES = config_nonsecret.S3_UPLOAD_DIRECTORY_CHOICES
+else:
+    S3_UPLOAD_DIRECTORY_CHOICES = [('','Root'),('','Same root')]
